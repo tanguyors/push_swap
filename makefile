ce44a5b1404429b3c58a4c096855bb81da402b04
Tanguy Orsini <torsini@c3r5s4.42luxembourg.lu>
2024-10-18 16:35:35 +0200

    v1

---
+++ makefile
@@ -0,0 +1,45 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: torsini <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/10/18 14:51:09 by torsini           #+#    #+#              #
+#    Updated: 2024/10/18 14:51:27 by torsini          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = push_swap
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+SRCS = push_swap.c
+OBJS = $(SRCS:.c=.o)
+
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+all: $(LIBFT) $(NAME)
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(NAME): $(OBJS) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS) -L$(LIBFT_DIR) -lft -o $(NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -I$(LIBFT_DIR) -c $< -o $@
+
+clean:
+	$(MAKE) -C $(LIBFT_DIR) clean
+	rm -f $(OBJS)
+
+fclean: clean
+	$(MAKE) -C $(LIBFT_DIR) fclean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ pushswap.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: torsini <torsini@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 10:00:00 by torsini           #+#    #+#             */
+/*   Updated: 2024/10/18 16:32:07 by torsini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft/libft.h"
+
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+}					t_node;
+
+typedef struct s_stack
+{
+	t_node			*top;
+	int				size;
+	int				chunk_size;
+	int				min;
+	int				max;
+}					t_stack;
+
+t_stack				*create_stack(void);
+void				push(t_stack *stack, int value);
+int					pop(t_stack *stack);
+void				free_stack(t_stack *stack);
+
+void				sa(t_stack *stack_a);
+void				sb(t_stack *stack_b);
+void				ss(t_stack *stack_a, t_stack *stack_b);
+void				pa(t_stack *stack_a, t_stack *stack_b);
+void				pb(t_stack *stack_b, t_stack *stack_a);
+void				ra(t_stack *stack_a);
+void				rb(t_stack *stack_b);
+void				rr(t_stack *stack_a, t_stack *stack_b);
+void				rra(t_stack *stack_a);
+void				rrb(t_stack *stack_b);
+void				rrr(t_stack *stack_a, t_stack *stack_b);
+
+#endif
