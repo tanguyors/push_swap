d4701a93ece5b47d58b5553c7ca1c4d654c447b0
root <root@DESKTOP-LNO871T>
2024-10-19 20:54:02 +0200

    v5

---
+++ stack_operation3.c
@@ -0,0 +1,8 @@
+#include "pushswap.h"
+
+void       rrr(t_stack *stack_a, t_stack *stack_b)
+{
+    rra(stack_a);
+    rrb(stack_b);
+}
+
--- stack_operations2.c
+++ stack_operations2.c
@@ -8,7 +8,7 @@ void	ra(t_stack *stack_a)
 	if (stack_a->size >= 2)
 	{
 		first = stack_a->top;              // Détache le premier élément (top)
-		stack_a->top = stack_a->top->next; // Le deuxième élément devient le nouveau top
+		stack_a->top = stack_a->top->next;// Le deuxième élément devient le nouveau top
 		last = stack_a->top;               // Trouver le dernier
 		while (last->next != NULL)
 		{
@@ -39,6 +39,44 @@ void	rb(t_stack *stack_b)
 }
 void	rr(t_stack *stack_a, t_stack *stack_b)
 {
-    ra(stack_a);
-    rb(stack_b);
+	ra(stack_a);
+	rb(stack_b);
+}
+
+void	rra(t_stack *stack_a)
+{
+	t_node	*current;
+	t_node	*last_element;
+
+	if (stack_a->size >= 2)
+	{
+		current = stack_a->top;
+		while (current->next->next != NULL)
+		{
+			current = current->next;
+		}
+		last_element = current->next;
+		current->next = NULL;
+		last_element->next = stack_a->top;
+		stack_a->top = last_element;
+	}
+}
+
+void	rrb(t_stack *stack_b)
+{
+	t_node *current;
+	t_node *last_element;
+
+	if (stack_b->size >= 2)
+	{
+		current = stack_b->top;
+		while (current->next->next != NULL)
+		{
+			current = current->next;
+		}
+		last_element = current->next;
+		current->next = NULL;
+		last_element->next = stack_b->top;
+		stack_b->top = last_element;
+	}
 }
